83c04701febca8eb2416ed76a3459b7e0c84cafc
Jakub Grzesiak <mr.kubag@gmail.com>
2020-06-02 14:07:50 +0200

    zoom test

--- game-planner-core/mainwindow.cpp
+++ game-planner-core/mainwindow.cpp
@@ -6,24 +6,47 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ///background = QPixmap(ui->graphicsView->width(),ui->graphicsView->height());
-    //filename = QFileDialog::getOpenFileName(this);
-    //qDebug() << filename;
-    background = QPixmap("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\b0.jpg");
+
     QRect rec = qApp->desktop()->availableGeometry();
     this->setGeometry(QStyle::alignedRect(
                           Qt::LeftToRight,
                           Qt::AlignCenter,
                           rec.size(),
                           rec));
-
     this->setFixedSize(rec.width()-100,rec.height()-50);
     ui->graphicsView->setFixedSize(this->size());
-    scena.addPixmap(background.scaledToHeight(this->height()-5));
-    ui->graphicsView->setScene(&scena);
+    this->ui->graphicsView->centerOn(this->width()/2,this->height()/2);
+
+    this->setBckg(QPixmap("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\b0.jpg"));
+    //background = QPixmap("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\b0.jpg");
+    //scena.addPixmap(background);
+    //scena.addPixmap(this->setOpacity(QPixmap("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\ic1.png"),0.4));
+    //scena.addPixmap(QPixmap("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\ic1.png"));
+    //ui->graphicsView->setScene(&scena);
     createActions();
     createMenus();
     obecne_pietro=0;
+    QIcon ikonka("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\op1.png");
+    ui->pushButton->setText("");
+    ui->pushButton->setIcon(ikonka);
+    ui->verticalLayoutWidget->setFixedSize(this->width()*0.1,this->height());
+    QPushButton *btn1 = new QPushButton("btn1");
+    btn1->setText("btn1");
+    ui->verticalLayout->addWidget(btn1);
+    QPushButton *btn2 = new QPushButton("btn2");
+    btn2->setText("btn2");
+    ui->verticalLayout->addWidget(btn2);
+    QPushButton *btn3 = new QPushButton("btn3");
+    btn3->setText("btn3");
+    ui->verticalLayout->addWidget(btn3);
+    QPushButton *btn4 = new QPushButton("btn4");
+    btn4->setText("btn4");
+    ui->verticalLayout->addWidget(btn4);
+    connect(btn1,&QAbstractButton::clicked,this,&MainWindow::btn1_clicked);
+    connect(btn2,&QAbstractButton::clicked,this,&MainWindow::btn2_clicked);
+
+    ui->graphicsView->setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
+    ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
 }
 
 MainWindow::~MainWindow()
@@ -62,25 +85,57 @@ void MainWindow::exit(){
     this->close();
 }
 
-QPixmap setOpacity(const QPixmap pix, double opacity){
+QPixmap MainWindow::setOpacity(const QPixmap pix, double opacity){
     QPixmap nowa;
-    QPainter *paint = new QPainter(&nowa);
-    paint->setOpacity(opacity);
-    paint->drawPixmap(0,0,pix);
-    delete paint;
+    QImage image(pix.size(), QImage::Format_ARGB32_Premultiplied);
+    image.fill(Qt::transparent);
+    QPainter p(&image);
+    p.setOpacity(opacity);
+    p.drawPixmap(0, 0, pix);
+    p.end();
+    nowa.convertFromImage(image);
     return nowa;
 }
 
-void MainWindow::resize(){
-
-}
-
-
 void MainWindow::on_pushButton_clicked()
 {
+    ui->graphicsView->resetTransform();
     obecne_pietro++;
-    background = QPixmap(("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\b" + QString::number(obecne_pietro) + ".jpg"));
-    scena.clear();
-    scena.addPixmap(background.scaledToHeight(this->height()-5));
-    ui->graphicsView->setScene(&scena);
+    this->setBckg(QPixmap(("C:\\Users\\MrKub\\Documents\\game-planner\\game-planner-core\\assets\\b" + QString::number(obecne_pietro) + ".jpg")));
+
 }
+
+void MainWindow::btn1_clicked(){
+    qreal factor = 0.9;
+    ui->graphicsView->scale(factor,factor);
+}
+
+void MainWindow::btn2_clicked(){
+    qreal factor = 1.1;
+    ui->graphicsView->scale(factor,factor);
+}
+
+void MainWindow::wheelEvent(QWheelEvent *event){
+    int angle = event->angleDelta().y();
+    qreal factor;
+    if (angle < 0) {
+        factor = 1.1;
+    } else {
+        factor = 0.9;
+    }
+    ui->graphicsView->scale(factor,factor);
+    //qDebug() << ui->graphicsView->width() << " " << ui->graphicsView->height();
+}
+
+void MainWindow::setBckg(QPixmap pix){
+    background = pix;
+    qreal x = qreal(ui->graphicsView->height())/qreal(background.height())*1/skala;
+    scena.clear();
+    //scena.addPixmap(background.scaledToHeight(this->height()-5,Qt::SmoothTransformation));
+    scena.addPixmap(background);
+    ui->graphicsView->setScene(&scena);
+    ui->graphicsView->scale(x,x);
+    skala = x;
+}
+
+void QGraphicsView::wheelEvent(QWheelEvent *event){} // przeladowanie żeby scroll nie scrollowal w dół
--- game-planner-core/mainwindow.h
+++ game-planner-core/mainwindow.h
@@ -10,8 +10,10 @@
 #include <QDesktopWidget>
 #include <QDebug>
 #include <QStyle>
-
+#include <QAbstractButton>
 #include <printer_core.h>
+#include <QWheelEvent>
+#include <QScrollBar>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -22,26 +24,34 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+
+    Ui::MainWindow *ui;
     QString filename;
     QGraphicsScene scena;
     QPixmap background;
+    qreal skala = 1;
     int obecne_pietro;
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-    void test();
-private:
-    Ui::MainWindow *ui;
-    void createActions();
-    void createMenus();
     QMenu *fileMenu;
     QActionGroup *alignmentGroup;
     QAction *openAct;
     QAction *exitAct;
-    void resize();
+
+    void setBckg(QPixmap pix);
+    QPixmap setOpacity(const QPixmap pix, double opacity);
+    void wheelEvent(QWheelEvent *event) override;
+    void createActions();
+    void createMenus();
+
 
 private slots:
     void open();
     void exit();
     void on_pushButton_clicked();
+    void btn1_clicked();
+    void btn2_clicked();
 };
 #endif // MAINWINDOW_H
