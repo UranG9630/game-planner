23a187d7caa85841ef6a93f8e62771b30735d59a
Jakub Grzesiak <mr.kubag@gmail.com>
2020-06-04 11:12:26 +0200

    testy rysowania ikon

---
+++ game-planner-core/scene_mouse.cpp
@@ -0,0 +1,6 @@
+#include "scene_mouse.h"
+
+scene_mouse::scene_mouse()
+{
+
+}
---
+++ game-planner-core/scene_mouse.h
@@ -0,0 +1,12 @@
+#ifndef SCENE_MOUSE_H
+#define SCENE_MOUSE_H
+
+#include <QObject>
+
+class scene_mouse : public QGraphicsScene
+{
+public:
+    scene_mouse();
+};
+
+#endif // SCENE_MOUSE_H
